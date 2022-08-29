0c3b4b9d6e9971e2917e2d1a29418ac61abb4e07
AdamSelki <adamselki@yahoo.com>
2022-08-29 12:31:48 -0700

    One-to-many relationship between Users and Albums

--- backend/db/models/album.js
+++ backend/db/models/album.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Album.belongsTo(models.User, {foreignKey: 'userId'});
     }
   }
   Album.init({
@@ -22,6 +23,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     imageUrl: {
       type: DataTypes.STRING
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   }, {
     sequelize,
