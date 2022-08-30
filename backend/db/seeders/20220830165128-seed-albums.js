4e21e188680f761394ab3ce65ed41579343f4db2
AdamSelki <adamselki@yahoo.com>
2022-08-30 10:22:58 -0700

    Update users, albums, and songs seeders

---
+++ backend/db/seeders/20220830165128-seed-albums.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Albums', [
+      {
+        userId: 1,
+        title: "Album1",
+        description: "This is an album of songs.",
+        imageUrl: null
+      },
+      {
+        userId: 2,
+        title: "Album2",
+        description: "This is an album of songs.",
+        imageUrl: null
+      }
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete('Albums', {
+     title : { [Op.in]: ['Album1', 'Album2'] }
+    }, { // truncate to reset autoincrement id
+      truncate: true,
+      cascade: true
+    });
+  }
+};
---
+++ backend/db/seeders/20220830170021-seed-songs.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Songs', [
+      {
+        albumId: 1,
+        userId: 1,
+        title: "Song1",
+        description: "This is a song.",
+        url: "audio url",
+        imageUrl: "image url"
+      },
+      {
+        albumId: 1,
+        userId: 1,
+        title: "Song2",
+        description: "This is a song.",
+        url: "audio url",
+        imageUrl: "image url"
+      },
+      {
+        albumId: 2,
+        userId: 2,
+        title: "Song3",
+        description: "This is a song.",
+        url: "audio url",
+        imageUrl: "image url"
+      },
+      {
+        albumId: 2,
+        userId: 2,
+        title: "Song4",
+        description: "This is a song.",
+        url: "audio url",
+        imageUrl: "image url"
+      }
+    ], {})
+  },
+
+  async down(queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete('Songs', {
+      title: { [Op.in]: ['Song1', 'Song2', 'Song3', 'Song4'] }
+    }, { // to fix auto increment
+      truncate: true,
+      cascade: true
+    });
+  }
+};
