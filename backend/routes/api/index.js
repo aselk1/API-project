772cd5564442f293ffdf6e804e02615d3c132b36
AdamSelki <adamselki@yahoo.com>
2022-08-30 09:50:04 -0700

    Songs route added in api folder

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const { User } = require('../../db/models');
 //session and user routes
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const songsRouter = require('./songs.js');
 const { restoreUser, requireAuth} = require('../../utils/auth.js');
 
 router.use(restoreUser); //connect restoreUser before any other middleware
@@ -12,6 +13,7 @@ router.use(restoreUser); //connect restoreUser before any other middleware
 //connect other routers
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/songs', songsRouter);
 
 // router.post('/test', function (req, res) { // test router
 //     res.json({ requestBody: req.body });
---
+++ backend/routes/api/songs.js
@@ -0,0 +1,21 @@
+// backend/routes/api/users.js
+const express = require('express')
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+//import validation stuff
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+
+router.get(
+    "/",
+    (req, res, next) => {
+
+    }
+)
+
+
+module.exports = router;
