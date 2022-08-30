0524041d92bda5e8a1152aeb6a4ca441aae23798
AdamSelki <adamselki@yahoo.com>
2022-08-30 10:56:20 -0700

    Get songs of current user

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,5 +1,6 @@
 // backend/routes/api/users.js
 const express = require('express')
+const jwt = require('jsonwebtoken');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Song } = require('../../db/models');
@@ -15,6 +16,22 @@ router.get(
     async (req, res, next) => {
         const Songs = await Song.findAll({});
         return res.json({Songs});
+    }
+);
+
+router.get(
+    "/current",
+    requireAuth,
+    async (req, res, next) => {
+        const { token } = req.cookies;
+        const payload = jwt.decode(token);
+        const id = payload.data.id
+        const Songs = await Song.findAll({
+            where: {
+                userId: id
+            }
+        });
+        return res.json({ Songs });
     }
 );
 
