ecdfae901ac9ca2da3f45da2b1a07861bd7e77aa
AdamSelki <adamselki@yahoo.com>
2022-08-31 13:46:45 -0700

    Delete album

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -14,7 +14,7 @@ router.get(
     "/",
     async (req, res, next) => {
         let Albums = await Album.findAll({})
-        res.json({Albums})
+        res.json({ Albums })
     }
 );
 
@@ -38,12 +38,13 @@ router.get(
     "/:albumId",
     async (req, res, next) => {
         let albumId = Number(req.params.albumId);
-        let album = await Album.findByPk( albumId,{
-            include:[
-                {model: User, attributes:['id', 'username', 'imageUrl'], as: 'Artist'},
-                { model: Song}
-            ]}
-            )
+        let album = await Album.findByPk(albumId, {
+            include: [
+                { model: User, attributes: ['id', 'username', 'imageUrl'], as: 'Artist' },
+                { model: Song }
+            ]
+        }
+        )
         if (!album) {
             let err = new Error("Album couldn't be found");
             err.status = 404;
@@ -57,19 +58,71 @@ router.post(
     "/",
     requireAuth,
     async (req, res, next) => {
-        let {title, description, imageUrl} = req.body;
+        let { title, description, imageUrl } = req.body;
         const { token } = req.cookies;
         const payload = jwt.decode(token);
         const id = payload.data.id;
-    if (!title) {
-        let err = new Error("Validation Error");
-        err.status = 400;
-        err.errors = {"title": "Album title is required"};
-        return next(err);
-    }
-        let album = await Album.create({title, description, imageUrl, userId: id});
+        if (!title) {
+            let err = new Error("Validation Error");
+            err.status = 400;
+            err.errors = { "title": "Album title is required" };
+            return next(err);
+        }
+        let album = await Album.create({ title, description, imageUrl, userId: id });
         res.json(album);
- })
+    });
+
+router.put(
+    "/:albumId",
+    requireAuth,
+    async (req, res, next) => {
+        let { title, description, imageUrl } = req.body;
+        let albumId = Number(req.params.albumId);
+        let album = await Album.findByPk(albumId);
+        if (!title) {
+            let err = new Error("Validation Error");
+            err.status = 400;
+            err.errors = { "title": "Album title is required" };
+            return next(err);
+        }
+        if (!album) {
+            let err = new Error("Album couldn't be found");
+            err.status = 404;
+            return next(err);
+        }
+        if (Number(req.user.dataValues.id) !== Number(album.userId)) {
+            let err = new Error('Forbidden');
+            err.status = 403;
+            return next(err);
+        };
+        album.update({title, description, imageUrl});
+        res.json(album)
+    }
+);
+
+router.delete(
+    "/:albumId",
+    requireAuth,
+    async (req, res, next) => {
+        let albumId = Number(req.params.albumId);
+        let album = await Album.findByPk(albumId);
+        if (!album) {
+            let err = new Error("Album couldn't be found");
+            err.status = 404;
+            return next(err);
+        }
+        if (Number(req.user.dataValues.id) !== Number(album.userId)) {
+            let err = new Error('Forbidden');
+            err.status = 403;
+            return next(err);
+        };
+        album.destroy();
+        res.json({
+            "message": "Successfully deleted",
+            "statusCode": 200
+        })
+    }
+);
 
 
 module.exports = router;
