273570897667cd8e1703e901b2e958f9a79f6128
AdamSelki <adamselki@yahoo.com>
2022-09-19 11:00:55 -0700

    Last main commit lol

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,12 +1,20 @@
-
 import { Switch, Route } from "react-router-dom";
 import Navigation from "../Navigation";
 import SignupFormPage from "../SignupFormPage";
 
-function HomePage({isLoaded}) {
+function HomePage({ isLoaded }) {
   return (
     <div>
-      <Navigation isLoaded={isLoaded} />
+      <div className="topBar"></div>
+      <div>
+        <div>
+          <img
+          className="logo"
+          src="https://d21buns5ku92am.cloudfront.net/26628/images/419679-1x1_SoundCloudLogo_cloudmark-f5912b-medium-1645807040.jpg">
+          </img>
+        </div>
+        <Navigation isLoaded={isLoaded} />
+      </div>
       {isLoaded && (
         <Switch>
           {/* <Route path="/signup">
