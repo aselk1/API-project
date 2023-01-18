6618a8d59a29f39488ca7ce0bef56ea4eb5f01c3
AdamSelki <adamselki@yahoo.com>
2023-01-18 13:32:13 -0800

    queue css

--- frontend/src/components/AudioPlayer/Queue.js
+++ frontend/src/components/AudioPlayer/Queue.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
 import { useDispatch, useSelector } from "react-redux";
+import logo from '../../images/blackLogo.png';
 import * as queueActions from "../../store/queue";
 
 function Queue({ setShowQueue, currentQueue }) {
@@ -20,30 +21,25 @@ function Queue({ setShowQueue, currentQueue }) {
     localStorage.setItem("queue", JSON.stringify(queue));
     await dispatch(queueActions.deleteSongFromQueue(queueId))
   }
-  // useEffect(() => {
-  //   //if menu is closed, return
-  //   // if (!showQueue) return;
-
-  //   //if menu if open, close it
-  //   const closeQueue = () => {
-  //     setShowQueue(false);
-  //   };
-
-  //   //add event listener for closeMenu
-  //   document.addEventListener("click", closeQueue);
-  //   //clean up function to remove the event listener
-  //   return () => document.removeEventListener("click", closeQueue);
-  // }, [setShowQueue]);
 
   return (
     <div className="queuePopup">
-      <div className="queueTitle">Queue</div>
+      <div className="queueTitle flexRow flexSpread">
+        My Queue
+        <img src={logo} className = "queueLogo"></img>
+      </div>
+      <div className="overflow queueSongs">
       {currentQueue.map((song) => (
-        <div className="flexRow flexSpread">
+        <div className="flexRow flexSpread bottomBorder">
           <div className="queueSong">{song.title}</div>
-          <i className="fa-duotone fa-x" id="queue" onClick={() => deleteSong(song.queueId)}></i>
+          <i
+            className="fa-duotone fa-x"
+            id="queue"
+            onClick={() => deleteSong(song.queueId)}
+          ></i>
         </div>
       ))}
+      </div>
     </div>
   );
 }
